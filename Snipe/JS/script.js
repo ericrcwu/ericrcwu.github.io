5fd8a30b68438f073432cc0134b38bd07666376e
darkS1D3 <bsd-13@BSD-13s-iMac.local>
2016-08-12 15:05:57 +0800

    gtttgtg

--- Snipe/JS/script.js
+++ Snipe/JS/script.js
@@ -1,11 +1,12 @@
 
 
 $(document).ready(function() {
-var money = 0;
+	var money = 0;
 
-$('#rrules').hide();
-$("#game").hide();
-//$("#shop").hide();
+	$('#rrules').hide();
+	$("#game").hide();
+	$("#shop").hide();
+	$("#finish_wave_1").hide();
 
 $("#start").click(function() {
 	var q1 = prompt("Are you sure?").toLowerCase();
@@ -14,7 +15,10 @@ $("#start").click(function() {
 		$("body").css({"-webkit-animation-play-state": "paused",
 		'background-image':'url(http://www.colorhexa.com/595959.png)'});
 		$("#game").show();
-	   }
+		
+		$(".zombie").animate({marginTop: "+=800px"}, 25000);
+		
+	}
 	 
 	else if (q1 === "no") {
 		alert("Ok");
@@ -38,11 +42,22 @@ $("#back").click(function() {
 
 //What happens when you click zombie1
 $('.zombie').click(function(){
-  $(this).toggle('explode');
-   money+= 5;
+   $(this).hide("explode");
+   money += 5;
    document.getElementById("moneyearned").innerHTML = money;
+   waveComplete();
 });
 
 
+//Function to check money earned 
+function waveComplete() {
+	if (money === 120) {
+		$("#finish_wave_1").show();
+		$("#gun").hide();
+		$("body").css({"background-image" : "url(http://www.beautycolorcode.com/653301-2880x1800.png)"});
+		$("#shop").show();
+	}
+
+}
 
 });
