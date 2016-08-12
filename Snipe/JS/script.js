6320a92a70d2d3275229a37ba4ecf679baeb4888
darkS1D3 <bsd-13@BSD-13s-iMac.local>
2016-08-12 09:19:01 +0800

    Day 2 and presentation

--- Snipe/JS/script.js
+++ Snipe/JS/script.js
@@ -1,20 +1,48 @@
-$("#rrules").hide();
-function start() {
+
+
+$(document).ready(function() {
+var money = 0;
+
+$('#rrules').hide();
+$("#game").hide();
+//$("#shop").hide();
+
+$("#start").click(function() {
 	var q1 = prompt("Are you sure?").toLowerCase();
 	if (q1 === "yes") {
-		var q2 = prompt("Did you read the rules?").toLowerCase();
-		if (q2 === "yes") {
-
-		}
-	}
+		$("#intro").hide();
+		$("body").css({"-webkit-animation-play-state": "paused",
+		'background-image':'url(http://www.colorhexa.com/595959.png)'});
+		$("#game").show();
+	   }
+	 
 	else if (q1 === "no") {
-
+		alert("Ok");
 	}
+
 	else {
 		alert("Please say yes or no next time, I don't speak gibberish.");
 	}
-}
-function rules() {
+
+});
+
+$("#rules").click(function() {
 	$("#intro").hide();
 	$("#rrules").show();
-}
+});
+
+$("#back").click(function() {
+	$("#intro").show();
+	$("#rrules").hide();	
+});
+
+//What happens when you click zombie1
+$('.zombie').click(function(){
+  $(this).toggle('explode');
+   money+= 5;
+   document.getElementById("moneyearned").innerHTML = money;
+});
+
+
+
+});
